91ea03f5177c663ff20dbc258c4f88129ea908b7
IsraelMerlyn <48464995+IsraelMerlyn@users.noreply.github.com>
2020-07-03 02:43:11 -0500

    final

--- Model/conexion.php
+++ Model/conexion.php
@@ -3,7 +3,7 @@
 	class Conexion{
 		public function conectar(){
 			try {
-				$conexion = new PDO('mysql:host=localhost;dbname=restaurant','root','1408');
+				$conexion = new PDO('mysql:host=e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=gu0njl1a8lkvxvq2','g4ne2uki5p1rcd01','ruunmrcm40g76949');
 				$conexion->exec('SET CHARACTER SET utf8');
 				$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
